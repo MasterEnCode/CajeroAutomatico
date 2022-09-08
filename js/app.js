a4af1be99e8ee6aedf879b702ac73810b9562aa1
Johan Neira <johan225@hotmail.es>
2022-09-07 20:42:51 -0500

    session & localstorage

--- js/app.js
+++ js/app.js
@@ -1,63 +1,83 @@
 const users = [
-  { nombre: "Johan", email: "Johan@hotmail.com", pass: "johan123*", saldo: 425 },
-  { nombre: "Mali", email: "Mali@hotmail.com", pass: "mali123*", saldo: 200 },
-  { nombre: "Maui", email: "Maui@hotmail.com", pass: "maui123*", saldo: 200 }
+  { nombre: "Johan", email: "johan@hotmail.com", pass: "johan123*", saldo: 425 },
+  { nombre: "Mali", email: "mali@hotmail.com", pass: "mali123*", saldo: 200 },
+  { nombre: "Maui", email: "maui@hotmail.com", pass: "maui123*", saldo: 200 }
 ];
+
+
 
 let actualUser = {};
 
-function checkPass(email, pass) {
+$(document).ready(function() {
+  storage();
+  checkUser();
+});
+
+storage = () => {
+  const userLS = localStorage.getItem('user');
+  actualUser = !userLS ? {} : JSON.parse(userLS);
+}
+
+checkUser = () => {
+  if (actualUser.nombre != undefined) {
+    $("#result").text(actualUser.nombre + " Bienvenid@");
+    $("#result").css("color", "white");
+    $("#Form").hide();
+    $("#logOut").show();
+    $("#account").show();
+    $("#saldo").text(actualUser.saldo);
+  } else {
+    $("#Form").show();
+    $("#logOut").hide();
+    $("#account").hide();
+  }
+}
+
+checkPass = (email, pass) => {
   let re =
     /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
   if (re.test(email)) {
     users.map((user) => {
-      if (user.email == email && user.pass == pass) {
+      if (user.email == email.toLowerCase() && user.pass == pass) {
         actualUser = user;
-      } else {
-        actualUser = {};
       }
     });
     return actualUser;
   }
 }
 
-function logIn(e) {
+
+logIn = (e) =>{
   e.preventDefault();
   $("#result").text("");
 
   let emailaddress = $("#email").val();
   let password = $("#pass").val();
   checkPass(emailaddress, password);
-
+  localStorage.setItem('user', JSON.stringify(actualUser));
   if (actualUser.nombre != undefined) {
-    $("#result").text(actualUser.nombre + " Bienvenid@");
-    $("#result").css("color", "white");
-    $("#Form").hide();
-    $("#account").show();
-    $("#saldo").text(actualUser.saldo);
+    checkUser()
   } else {
     $("#msj").text("Usuario o contraseña incorrectos");
     $("#msj").css("color", "red");
   }
   return false;
 }
-$("#login").bind("click", logIn);
 
-function clear() {
+clear = () => {
   $("#ingresarMonto").val("");
   $("#retirarMonto").val("");
   return false;
 }
 
-function mostrarSaldo() {
+mostrarSaldo = () => {
   $("#saldo").css("color", "green");
   $("#saldo").fadeTo(100, 0.1).fadeTo(500, 1.0);
   $("#saldo").css("color", "white");
   return false;
 }
 
-function consultarSaldo(saldo) {
+consultarSaldo = (saldo) => {
   actualUser.saldo = saldo;
   $("#saldo").text(actualUser.saldo);
   $("#saldo").css("color", "green");
@@ -67,7 +87,7 @@ function consultarSaldo(saldo) {
   return false;
 }
 
-function ingresarMonto() {
+ingresarMonto = () => {
   let monto = parseInt($("#ingresarMonto").val());
   if (!monto) {
     $("#transaction").text("Debe ingresar un monto y que sea superior a 0");
@@ -89,13 +109,11 @@ function ingresarMonto() {
       }
     });
   }
-
-
   clear();
   return actualUser;
 }
 
-function retirarMonto() {
+retirarMonto = () =>{
   let monto = parseInt($("#retirarMonto").val());
   if (!monto) {
     $("#transaction").text("Debe ingresar un monto y que sea superior a 0");
@@ -121,6 +139,18 @@ function retirarMonto() {
   return actualUser;
 }
 
+logOut  = () => {
+  localStorage.clear();
+  storage();
+  checkUser()
+}
+
+$("#login").bind("click", logIn);
+$("#logOut").bind("click", logOut);
+
 $("#consultarMontoBtn").bind("click", mostrarSaldo);
 $("#ingresarMontoBtn").bind("click", ingresarMonto);
 $("#retirarMontoBtn").bind("click", retirarMonto);
+
+
+
